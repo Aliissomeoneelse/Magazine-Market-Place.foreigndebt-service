596cc4d958138590573dace1ff55985a849f52ad
Aliissomeoneelse <aliwulug1307@gmail.com>
2023-07-12 20:00:07 +0500

    added

---
+++ src/test/java/com/company/foreigndebt/ForeignDebtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.foreigndebt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForeignDebtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
